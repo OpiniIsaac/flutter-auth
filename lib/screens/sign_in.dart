30edc92f45f0fc15c11a85e2ff87a4272d91edab
OpiniIsaac <isaacopini8@gmail.com>
2023-12-21 06:23:52 +0300

    added images

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -5,34 +5,60 @@ class SignIn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-     body: SafeArea(
-       child: Padding(
-         padding: EdgeInsets.all(16),
-         child: Column(
-          children: [
-            Icon(Icons.lock,size: 60),
-              SizedBox(height: 24,),
-            Text('Welcome Back',style: TextStyle(fontSize: 32)),
-            SizedBox(height: 24,),
-            TextField(
-              decoration: InputDecoration(
-                hintText: "Email",
-                border: OutlineInputBorder()
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Icon(Icons.lock, size: 60),
+              SizedBox(
+                height: 24,
               ),
-            ),
-            SizedBox(height: 24,),
-            TextField(
-              decoration: InputDecoration(
-                hintText: "Password",
-                border: OutlineInputBorder()
+              Text('Welcome Back', style: TextStyle(fontSize: 32)),
+              SizedBox(
+                height: 24,
               ),
-            ),
-           
-          ],
-         ),
-       ),
-     ),
+              TextField(
+                decoration: InputDecoration(
+                    hintText: "Email", border: OutlineInputBorder()),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    hintText: "Password", border: OutlineInputBorder()),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              Text('Forgot Password?'),
+              SizedBox(
+                height: 24,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: FilledButton(
+                    child: Text('sign in ',style: TextStyle(
+                      fontSize: 16
+                    ),),
+                    onPressed: () {},
+                    style: FilledButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      padding: EdgeInsets.symmetric( vertical:16)
+                    ),
+                  )),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
